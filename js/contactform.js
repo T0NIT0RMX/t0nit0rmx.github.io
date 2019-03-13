17eff43e43a80e981f2a87cea27751c9a721d4c6
T0NIT0RMX <t0nit0rmx@gmail.com>
2019-03-13 23:17:13 +0100

    ToneZ teaser

---
+++ js/contactform.js
@@ -0,0 +1,121 @@
+
+(function ($) {
+    "use strict";
+
+
+    /*==================================================================
+    [ Focus Contact2 ]*/
+    $('.input2').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+            
+  
+    
+    /*==================================================================
+    [ Validate ]*/
+    var name = $('.validate-input input[name="name"]');
+    var email = $('.validate-input input[name="email"]');
+    var message = $('.validate-input textarea[name="message"]');
+
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        if($(name).val().trim() == ''){
+            showValidate(name);
+            check=false;
+        }
+
+
+        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+            showValidate(email);
+            check=false;
+        }
+
+        if($(message).val().trim() == ''){
+            showValidate(message);
+            check=false;
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input2').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+       });
+    });
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    
+
+})(jQuery);
+
+
+var $form = $('form#test-form'),
+    url = 'https://script.google.com/macros/s/AKfycbwimKZEbVnC3gc5mU-_PaNj1SRz_O2R4vgf3HcaWy2h-fhPgCRd/exec'
+
+$('#submit-form').on('click', function(e) {
+    $('.loadring').toggleClass( "lds-ring" )
+  e.preventDefault();
+  var resetBtn = document.getElementById("submit-form");
+    resetBtn.disabled = true;
+    var timeStampInMs = Math.floor((new Date()).getTime() / 1000);
+            console.log(timeStampInMs);
+  var a=document.forms["Form"]["age"].value;
+  if (a!="")
+  {         
+            document.getElementById("test-form").reset();
+            return true;
+            
+
+            
+        }
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serialize()+ '&timestamp=' + timeStampInMs,
+    success: function(data)          
+     {   
+        id = data[0];              
+        vname = data[1];
+     },
+    complete: function (data) {
+      document.getElementById("test-form").reset();
+        $('.loadring').toggleClass( "lds-ring" )
+        document.getElementById("submit-form").innerHTML = "Submitted successfuly"
+        setTimeout(suiteTraitement, 5000)
+        
+     }
+    });
+})
+
+function suiteTraitement()
+{
+    var resetBtn = document.getElementById("submit-form");
+    resetBtn.disabled = false;
+        document.getElementById("submit-form").innerHTML = "Submit"
+}
+
+
+
---
+++ js/donatebutton.js
@@ -0,0 +1,84 @@
+var donationbutton = $('.donation');
+var windowsbutton = $('.windows');
+
+var body = document.body;
+
+$('#donate-button').click(function(){
+    donationbutton.addClass('active');
+    body.classList.toggle('noscroll');
+});
+
+$('.close-mymodal').click(function(){
+    donationbutton.removeClass('active');
+    windowsbutton.removeClass('active');
+    body.classList.toggle('noscroll');
+});
+
+$('#windows-button').click(function(){
+    windowsbutton.addClass('active');
+    body.classList.toggle('noscroll');
+});
+
+
+$('#donatechoice1').click(function(){
+     
+    $('#donatebutton1').addClass('activebutton');
+    $('#donatebutton2').removeClass('activebutton');
+    $('#donatebutton3').removeClass('activebutton');
+        $('#donatebutton4').removeClass('activebutton');
+
+    document.querySelector("[name=amount]").value = 50;
+});
+
+$('#donatechoice2').click(function(){
+     
+    $('#donatebutton1').removeClass('activebutton');
+    $('#donatebutton2').addClass('activebutton');
+    $('#donatebutton3').removeClass('activebutton');
+        $('#donatebutton4').removeClass('activebutton');
+
+    document.querySelector("[name=amount]").value = 20;
+
+});
+
+$('#donatechoice3').click(function(){
+     
+    $('#donatebutton1').removeClass('activebutton');
+    $('#donatebutton2').removeClass('activebutton');
+    $('#donatebutton3').addClass('activebutton');
+    $('#donatebutton4').removeClass('activebutton');
+    document.querySelector("[name=amount]").value = 10;
+});
+
+$('#donatechoice4').click(function(){
+     
+    $('#donatebutton1').removeClass('activebutton');
+    $('#donatebutton2').removeClass('activebutton');
+    $('#donatebutton3').removeClass('activebutton');
+    $('#donatebutton4').addClass('activebutton');
+    document.querySelector("[name=amount]").value = document.querySelector("[name=nameyourprice]").value;
+});
+
+$("#donatechoice4").on("change keyup paste", function(){
+    document.querySelector("[name=amount]").value = document.querySelector("[name=nameyourprice]").value;
+})
+
+
+
+$('#windows-button').on('click', function () {
+    $.ajax({
+        url: 'https://t0nit0rmx.github.io/ld/ToneZ_v1.0.zip',
+        method: 'GET',
+        xhrFields: {
+            responseType: 'blob'
+        },
+        success: function (data) {
+            var a = document.createElement('a');
+            var url = window.URL.createObjectURL(data);
+            a.href = url;
+            a.download = 'ToneZ_v1.0.zip';
+            a.click();
+            window.URL.revokeObjectURL(url);
+        }
+    });
+});
