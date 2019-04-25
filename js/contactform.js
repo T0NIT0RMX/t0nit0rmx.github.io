008aff9c6c9910af6407b04ad9b320b6202b01b1
T0NIT0RMX <t0nit0rmx@gmail.com>
2019-04-25 20:00:14 +0200

    tests

--- js/contactform.js
+++ js/contactform.js
@@ -128,34 +128,8 @@ function suiteTraitement()
 
 
 
-$(document).ready( function () {
-    var $form = $('#mc-embedded-subscribe-form');
-    if ( $form.length > 0 ) {
-        $('form input[type="submit"]').bind('click', function ( event ) {
-            if ( event ) event.preventDefault();
-            register($form);
-        });
-    }
-});
 
-function register($form) {
-    $.ajax({
-        type: $form.attr('method'),
-        url: $form.attr('action'),
-        data: $form.serialize(),
-        cache       : false,
-        dataType    : 'json',
-        contentType: "application/json; charset=utf-8",
-        error       : function(err) { alert("Could not connect to the registration server. Please try again later."); },
-        success     : function(data) {
-            if (data.result != "success") {
-                // Something went wrong, do something to notify the user. maybe alert(data.msg);
-            } else {
-                // It worked, carry on...
-            }
-        }
-    });
-}
+
 
 
 
